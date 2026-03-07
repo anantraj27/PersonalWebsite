077444308f8887e0a14f9b8665bfd9ef7ec15e13
anantraj27 <anantraj5236@gmail.com>
2026-03-08 03:23:41 +0530

    backend fix

--- src/controller/noteController.js
+++ src/controller/noteController.js
@@ -1,28 +1,37 @@
 import db from '../config/db.js';
 
 export const getNotes = async (req, res) => {
-    try {
-        const userid = req.user.id;
-        let page = Number(req.query.page);
-        let limit = Number(req.query.limit);
-        const offset = (page - 1) * limit;
+   try {
 
-        const result = await db.query(
-            'SELECT *,COUNT(*) OVER() FROM notes  WHERE user_id =$1 ORDER BY updated_at  DESC OFFSET $2 LIMIT $1 ',
-            [userid, 2, 2]
-        );
-        console.log(result);
-        return res.json({
-            success: true,
-            message: 'saved succesfully ..🐘',
-            notes: result.rows,
-            count: result.rows[0].count,
-        });
-    } catch (error) {
-        res.status(500).json({ success: false });
-    }
+      const userid = req.user.id;
+      const page = Number(req.query.page);
+      const limit = Number(req.query.limit);
+
+      const offset = (page - 1) * limit;
+
+      const result = await db.query(
+         `SELECT *, COUNT(*) OVER() AS count
+          FROM notes
+          WHERE user_id = $1
+          ORDER BY updated_at DESC
+          OFFSET $2
+          LIMIT $3`,
+         [userid, offset, limit]
+      );
+
+      return res.json({
+         success: true,
+         notes: result.rows,
+         count: result.rows.length ? result.rows[0].count : 0,
+      });
+
+   } catch (err) {
+
+      console.error(err);
+      res.status(500).json({ success:false });
+
+   }
 };
-
 export const editNotes = async (req, res) => {
     try {
         const userid = req.user.id;
